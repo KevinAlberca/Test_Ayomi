59ce25feb55d86b13c18bca2ed8ab8fa020420ec
Kevin ALBERCA <KevinAlberca@users.noreply.github.com>
2020-12-23 12:10:19 +0100

    Update base settings

--- ayomi/settings/base.py
+++ ayomi/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '..')
 
 
 # Quick-start development settings - unsuitable for production
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'ayomi.urls'
 
 TEMPLATES = [
     {
