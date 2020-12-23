9c3f135af7b812907526b80f9da5a62f66d4f94a
Kevin ALBERCA <KevinAlberca@users.noreply.github.com>
2020-12-23 13:46:54 +0100

    Create login view

---
+++ authentication/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from authentication.forms import LoginForm
+# Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.data.get('username'), password=form.data.get('password'))
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                form.add_error('password', 'Account not found.')
+    else:
+        form = LoginForm()
+    return render(request, 'authentication/login.html', {'form': form})
+
+
+@login_required(login_url='/login')
+def home_view(request):
+    return render(request, 'authentication/home.html')
--- ayomi/urls.py
+++ ayomi/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from authentication.views import login_view, home_view
 
 urlpatterns = [
+    path('', home_view),
+    path('login', login_view),
     path('admin/', admin.site.urls),
 ]
